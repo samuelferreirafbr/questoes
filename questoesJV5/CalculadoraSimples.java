6e2ed42afa4ea36c53fe8ad0475182ca8357753f
Samuel Ferreira <samuelferreiradasilva1570@gmail.com>
2025-05-08 15:41:50 -0300

    Adicionando a pasta questoesJV5 ao repositório

---
+++ questoesJV5/CalculadoraSimples.java
@@ -0,0 +1,14 @@
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+public class CalculadoraSimples {
+    public static void main(String args[]){
+
+    String Operação = JComboBox("Escolha a operação: ");
+
+    int valorA = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor A: "));
+    int valorB = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor B: "));
+
+
+    }
+}
---
+++ questoesJV5/programaPrincipal.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class programaPrincipal {
+
+    public static void main(String args[]){
+        	
+        String nome = JOptionPane.showInputDialog("Digite o nome: ");
+        String sobreNome = JOptionPane.showInputDialog("Digite o sobrenome: ");
+
+        String nomeCompleto = nome + " " + sobreNome;
+
+        JOptionPane.showMessageDialog(null, "Nome Completo: " + nomeCompleto, "Informação", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
